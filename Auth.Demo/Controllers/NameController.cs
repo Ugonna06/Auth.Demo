a74ca9ea36dce8cfbe6ad604b2c95a9d3010f3c6
Ugonna Nnadozie <ugonnadozie06@gmail.com>
2022-01-05 12:15:18 +0100

    first commit

---
+++ Auth.Demo/Controllers/NameController.cs
@@ -0,0 +1,52 @@
+﻿using Auth.Demo.AuthenticationMgt.AuthInterface;
+using Auth.Demo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth.Demo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager _jwtAuthenticationManager;
+
+        public NameController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            _jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = _jwtAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
+ 
